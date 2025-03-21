e81cf56d867c0e3845544ace1d18b9fe5ab92e84
faizachwd <faiza.chowdhury789@gmail.com>
2025-03-20 20:29:42 -0400

    update script.js

--- script.js
+++ script.js
@@ -24,28 +24,76 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 let collision;
 
 // Fetch GeoJSON from URL and store response
-fetch('')
+fetch('https://raw.githubusercontent.com/faizachwd/ggr472-lab4/refs/heads/main/data/pedcyc_collision_06-21.geojson')
     .then(response => response.json())
     .then(response => {
-        console.log(response); //Check response in console
         collision = response; // Store geojson as variable using URL from fetch response
+
+        map.on('load', () => {
+
+            let evnresult = turf.envelope(collision);
+            let bbox = turf.transformScale(evnresult, 1.1);
+
+            console.log('collision', collision)
+
+            // accessing and store the bounding box coordinates as an array variable
+            let bbox_coords = [
+                bbox.geometry.coordinates[0][0][0],
+                bbox.geometry.coordinates[0][0][1],
+                bbox.geometry.coordinates[0][2][0],
+                bbox.geometry.coordinates[0][2][1]
+            ]
+
+            console.log('bbox coord', bbox_coords)
+
+            // using bounding box coordinates as argument in the turf hexgrid function
+            let hexdata = turf.hexGrid(bbox_coords, 0.5, { units: "kilometers" });
+
+            console.log('hexdata', hexdata)
+
+            let collishex = turf.collect(hexdata, collision, '_id', 'values');
+            console.log('collishex', collishex)
+
+            let maxcollis = 0;
+
+            collishex.features.forEach((feature) => {
+                feature.properties.COUNT = feature.properties.values.length
+                if (feature.properties.COUNT > maxcollis) {
+                    maxcollis = feature.properties.COUNT
+                }
+            });
+            
+            console.log('maxcollis',maxcollis)
+            
+            map.addSource('hexgrid', {
+                type: 'geojson',
+                data: collishex
+            })
+
+            map.addLayer({
+                'id': 'hex_layer',
+                'source': 'hexgrid',
+                'type': 'fill',
+                'paint': { 
+                    "fill-color": [
+                        "step",
+                        ["get", "COUNT"],
+                        "#D59967",
+                        10, "#EE6055",
+                        25, "#D59967",
+                        maxcollis, ''
+                    ],
+                    "fill-opacity": 0.8
+                },
+                filter: ["!=", 'COUNT', 0],
+            })
+
+        });
+
     });
 
-//      Convert the response to JSON format and then store the response in your new variable
 
-
-
-/*--------------------------------------------------------------------
-    Step 3: CREATE BOUNDING BOX AND HEXGRID
---------------------------------------------------------------------*/
-//HINT: All code to create and view the hexgrid will go inside a map load event handler
-//      First create a bounding box around the collision point data
-//      Access and store the bounding box coordinates as an array variable
-//      Use bounding box coordinates as argument in the turf hexgrid function
-//      **Option: You may want to consider how to increase the size of your bbox to enable greater geog coverage of your hexgrid
-//                Consider return types from different turf functions and required argument types carefully here
-
-
+// creating a bounding box around the collision point data and scaling it up
 
 /*--------------------------------------------------------------------
 Step 4: AGGREGATE COLLISIONS BY HEXGRID
